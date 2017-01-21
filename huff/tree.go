379248c8cf927a897bd09f23629111f6b3854610
MURAOKA Taro <koron.kaoriya@gmail.com>
2017-01-22 00:33:56 +0900

    initialize tree

--- huff/tree.go
+++ huff/tree.go
@@ -2,14 +2,29 @@ package huff
 
 // Tree represents a huffman tree.
 type Tree struct {
+	// Leaves store
 	Leaves []int32
+
+	// Nodes store links to other nodes (0 or greater) or leaves (minus).
 	Nodes  []int
 }
 
 // New creates a empty huffman tree with size.
 func New(n int) *Tree {
+	leaves := make([]int32, n)
+	for i := range leaves {
+		leaves[i] = int32(i)
+	}
+
+	nodes := make([]int, (n-1)*2)
+	for i := 0; i < n-1; i++ {
+		nodes[i] = -i - 1
+		nodes[i+1] = (i + 1) * 2
+	}
+	nodes[len(nodes)-1] = -n - 1
+
 	return &Tree{
-		Leaves: make([]int32, n),
-		Nodes:  make([]int, (n-1)*2),
+		Leaves: leaves,
+		Nodes:  nodes,
 	}
 }
